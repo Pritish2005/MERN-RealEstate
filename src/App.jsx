3f67f4ddd982e513704d5a68c80a452382a8accf
Pritish2005 <pritishmahajan12@gmail.com>
2024-06-07 00:08:50 +0530

    Create Pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
+import Signin from './components/Signin'
+import Signout from './components/Signout'
+import About from './components/About'
+import Profile from './components/Profile'
 
 function App() {
   console.log('jello')
   return (
     <>
-      <h1 className=' text-red-800'>Jello</h1>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<Signin/>} />
+      <Route path='/sign-out' element={<Signout/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/profile' element={<Profile/>} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
