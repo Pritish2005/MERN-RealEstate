4d1f6a267e358053db5a98fc0fe4c3fc754dd929
Pritish2005 <pritishmahajan12@gmail.com>
2024-06-09 00:05:22 +0530

    Commit to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDb')
+})
 
 const app=express();
 
