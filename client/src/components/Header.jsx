1f641f4383485dccb44db2b0815d8520f7365bc8
Pritish2005 <pritishmahajan12@gmail.com>
2024-06-16 21:15:09 +0530

    Profile page UI creation

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,9 @@ function Header() {
         <Link to='/about'> <li className=' hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
         {
           currentUser?(
-            <img className=' rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
+            <Link to='/profile'>
+              <img className=' rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
+            </Link>
           ):(
             <Link to='/sign-in'><li className=' text-slate-700 hover:underline'>Sign-in</li></Link> 
           )
